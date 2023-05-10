2836cca6d7bdabed5dd495bb95dcb7972412bfab
PeteHaitch <peter.hickey@gmail.com>
2023-05-10 15:44:09 +1000

    Add compare_outputs.sh (for internal use only)

---
+++ code/compare_outputs.sh
@@ -0,0 +1,8 @@
+# Comapre GEO outputs for G000187_Bhavana_Yasmin_heart_paper to
+# G000187_Bhavana_Yasmin.
+# For internal use only.
+# Peter Hickey
+# 2023-05-10
+
+diff ../G000187_Bhavana_Yasmin/GEO/SCE/Macrophage_heart_paper/ GEO/SCE/Macrophage_heart_paper/
+diff ../G000187_Bhavana_Yasmin/GEO/SCE/Treg_heart_paper/ GEO/SCE/Treg_heart_paper/
